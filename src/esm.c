29ffc4e67b3bf251f15fbe9280d2b5710b136131
Will Harris <will@greatlibrary.net>
2010-02-07 18:38:42 +0000

    Use callback query function.

--- src/esm.c
+++ src/esm.c
@@ -104,15 +104,30 @@ esm_Index_fix(esm_IndexObject* self) {
     Py_RETURN_NONE;
 }
 
+ac_error_code
+append_result(void* data, ac_result* result)
+{
+    PyObject*   result_list = (PyObject*) data;
+    PyObject*   result_tuple = NULL;
+    
+    result_tuple = Py_BuildValue("((ii)O)", result->start,
+                                            result->end,
+                                            (PyObject*) result->object);
+    
+    if (PyList_Append(result_list, result_tuple)) {
+        Py_DECREF(result_tuple);
+        return AC_FAILURE;
+    }
+    
+    Py_DECREF(result_tuple);
+    return AC_SUCCESS;
+}
+
 static PyObject*
 esm_Index_query(esm_IndexObject* self, PyObject* args) {
     char*         phrase = NULL;
     int           length = 0;
-    ac_list*      results = NULL;
-    ac_list_item* result_item = NULL;
-    ac_result*    result = NULL;
     PyObject*     result_list = NULL;
-    PyObject*     result_tuple = NULL;
     
     if (self->index->index_state != AC_INDEX_FIXED) {
         PyErr_SetString(PyExc_TypeError, "Can't call query before fix");
@@ -123,40 +138,18 @@ esm_Index_query(esm_IndexObject* self, PyObject* args) {
         return NULL;
     }
     
-    if ( ! (results = ac_list_new())) {
-        return PyErr_NoMemory();
-    }
-    
-    if (ac_index_query(self->index,
-                       (ac_symbol*) phrase,
-                       (ac_offset) length,
-                       results) != AC_SUCCESS) {
-        ac_result_list_free(results);
-        return PyErr_NoMemory();
-    }
-    
     if ( ! (result_list = PyList_New(0))) {
-        ac_result_list_free(results);
         return PyErr_NoMemory();        
     }
     
-    result_item = results->first;
-    while (result_item) {
-        result = (ac_result*) result_item->item;                                
-        result_tuple = Py_BuildValue("((ii)O)", result->start,
-                                                result->end,
-                                                (PyObject*) result->object);
-        
-        if (PyList_Append(result_list, result_tuple)) {
-            Py_DECREF(result_tuple);
-            return PyErr_NoMemory();
-        }
-        
-        Py_DECREF(result_tuple);
-        result_item = result_item->next;
+    if (ac_index_query_cb(self->index,
+                          (ac_symbol*) phrase,
+                          (ac_offset) length,
+                          append_result,
+                          result_list) != AC_SUCCESS) {
+        Py_DECREF(result_list);
+        return PyErr_NoMemory();
     }
-    
-    ac_result_list_free(results);
     
     return result_list;
 }
